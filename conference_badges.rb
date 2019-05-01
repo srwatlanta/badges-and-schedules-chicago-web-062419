5a308d7a2b8468ca57614344fb70c48e6734534b
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 02:18:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
-end 
+end
 
 def batch_badge_creator(attendees)
   badges=[]
@@ -8,23 +8,23 @@ def batch_badge_creator(attendees)
   badges.push("Hello, my name is #{badge}.")
   end
   return badges
-  
-end 
+
+end
 
 def assign_rooms(speakers)
   assign=[]
-  
+
   speakers.each_with_index{|speaker,index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
     }
    return assign
-end 
+end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  
+
   assign_rooms(attendees).each do |badge|
     puts badge
   end
