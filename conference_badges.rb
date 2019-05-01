7db530a6bb65f7df11a46037483580362832e878
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 01:48:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-
+def batch_badge_creator (names)
+  names.each do | name |
+    badge_maker(name)
 end
 
 def assign_rooms
