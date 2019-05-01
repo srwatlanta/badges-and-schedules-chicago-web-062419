1e02d8314b539401f5db8390dd7be37ad9f403a6
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-01 02:07:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ end
 room_message
 end
 
-def printer(attendees)
-  puts batch_badge_creator(attendees[0])
-  puts assign_rooms(attendees[0])
+def printer(attendees[0])
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
